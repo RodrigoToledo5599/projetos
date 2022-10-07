ca04bc6ae1986fb9713fc01ac08a6a68553a263c
rodrigo4532 <rodrigotmt89@gmail.com>
2022-10-07 19:54:28 -0300

    styling tasks.html

--- todolist/TodoList/urls.py
+++ todolist/TodoList/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('',TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
     path('create-task/',TaskCreate.as_view(),name='task_form'),
-    
     path('delete-task/<int:pk>/',DeleteView.as_view(),name='task_delete')
 ]
