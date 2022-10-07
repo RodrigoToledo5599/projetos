717e353969adf442de9af2125eb2a9a0ff87193b
rodrigo4532 <rodrigotmt89@gmail.com>
2022-10-07 15:58:13 -0300

    first commit

---
+++ todolist/TodoList/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls.base import reverse_lazy
+from .models import *
+
+class TaskList(ListView):
+    model = Task
+    template_name="TodoList/tasks.html"
+    context_object_name="tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name="TodoList/task.html"
+    context_object_name="task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task_update')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
